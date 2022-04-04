0d3f54704621d511575c1c0f49eddb01ada37fc1
Nick Tyler <tylern@lbl.gov>
2022-04-04 17:30:31 -0400

    Saving todays work

--- GlacierStats/GlacierStatsGPU.py
+++ GlacierStats/GlacierStatsGPU.py
@@ -11,6 +11,7 @@
 
 import cupy as cp
 import cudf
+from cuml.metrics import pairwise_distances
 import numpy as np
 import numpy.linalg as linalg
 import pandas as pd
@@ -22,26 +23,30 @@ from tqdm import tqdm
 import random
 
 
+# In[4]:
+
+def helloworld():
+    print("hello world")
+
+
 # covariance function definition
 def covar(t, d, r):
     h = d / r
     if t == 1:  # Spherical
-        c = 1 - h * (1.5 - 0.5 * np.square(h))
+        c = 1 - h * (1.5 - 0.5 * cp.square(h))
         c[h > 1] = 0
     elif t == 2:  # Exponential
-        c = np.exp(-3 * h)
+        c = cp.exp(-3 * h)
     elif t == 3:  # Gaussian
-        c = np.exp(-3 * np.square(h))
+        c = cp.exp(-3 * cp.square(h))
     return c
 
 def sortQuadrantPoints(quad_array, quad_count, rad):
-    quad_array['Dist'] = cp.linalg.norm(quad_array[["norm_X","norm_Y"]].values)
+    quad_array['Dist'] = cp.linalg.norm(quad_array[["X","Y"]].values, axis = 1)
+    quad_array = quad_array[quad_array.Dist < rad] # delete points outside of radius
     quad_array = quad_array.sort_values('Dist',ascending = True) # sort array by distances
     # select the number of points in each quadrant up to our quadrant count
     smallest = quad_array.iloc[:quad_count]
-    print(quad_count, smallest.shape)
-    # delete points outside of our radius
-    smallest = smallest[smallest['Dist'] > rad]
     return smallest
     
 def nearestNeighborSearch(rad, count, loc, data):
@@ -51,27 +56,31 @@ def nearestNeighborSearch(rad, count, loc, data):
     # wipe coords for re-usability 
     #coords = np.empty_like(data)
     coords = data.copy()
-
+    #coords[:] = data
     # standardize our quadrants (create the origin at our location point)
-    coords['norm_X'] = coords['X'] - locx
-    coords['norm_Y'] = coords['Y'] - locy
- 
+    coords.X -= locx
+    coords.Y -= locy
+    
     # Number of points to look for in each quadrant, if not fully divisible by 4, round down
     quad_count = count//4
     
     # sort coords of dataset into 4 quadrants relative to input location
     final_quad = []
-    final_quad.append(coords[(coords.norm_X >= 0) & (coords.norm_Y >= 0)])
-    final_quad.append(coords[(coords.norm_X < 0) & (coords.norm_Y < 0)])
-    final_quad.append(coords[(coords.norm_X >= 0) & (coords.norm_Y < 0)])
-    final_quad.append(coords[(coords.norm_X < 0) & (coords.norm_Y >= 0)])
+    final_quad.append(coords[(coords.X >= 0) & (coords.Y >= 0)])
+    final_quad.append(coords[(coords.X < 0) & (coords.Y < 0)])
+    final_quad.append(coords[(coords.X >= 0) & (coords.Y < 0)])
+    final_quad.append(coords[(coords.X < 0) & (coords.Y >= 0)])
     
     # Gather distance values for each coord from point and delete points outside radius
     fcoord = []
     for quad in final_quad:
         fcoord.append(sortQuadrantPoints(quad, quad_count, rad))
     
+    # add all quadrants back together for final dataset
     near = cudf.concat(fcoord)
+    # unstandardize data back to original form
+    near.X += locx
+    near.Y += locy
     return near
 
 
@@ -123,19 +132,67 @@ def pred_grid(xmin, xmax, ymin, ymax, pix):
 
 # rotation matrix (Azimuth = major axis direction)
 def Rot_Mat(Azimuth, a_max, a_min):
-    theta = (Azimuth / 180.0) * np.pi
-    Rot_Mat = np.dot(
-        np.array([[1 / a_max, 0], [0, 1 / a_min]]),
-        np.array(
+    theta = (Azimuth / 180.0) * cp.pi
+    Rot_Mat = cp.dot(
+        cp.array([[1 / a_max, 0], [0, 1 / a_min]]),
+        cp.array(
             [
-                [np.cos(theta), np.sin(theta)],
-                [-np.sin(theta), np.cos(theta)],
+                [cp.cos(theta), cp.sin(theta)],
+                [-cp.sin(theta), cp.cos(theta)],
             ]
         ),
     )
     return Rot_Mat
 
 
+
+# covariance model
+def krig_cov(q, vario):
+    # unpack variogram parameters
+    Azimuth = vario[0]
+    nug = vario[1]
+    nstruct = vario[2]
+    vtype = vario[3]
+    cc = vario[4]
+    a_max = vario[5]
+    a_min = vario[6]
+    
+    c = -nug # nugget effect is made negative because we're calculating covariance instead of variance
+    for i in range(nstruct):
+        mat = cp.matmul(q, Rot_Mat(Azimuth, a_max[i], a_min[i]))
+        
+        #print(mat)
+        
+        # covariances between measurements
+        d = pairwise_distances(mat,mat)
+        c = c + covar(vtype[i], d, 1) * cc[i]
+    return c
+
+# covariance model
+def array_cov(q1, q2, vario):
+    # unpack variogram parameters
+    Azimuth = vario[0]
+    nug = vario[1]
+    nstruct = vario[2]
+    vtype = vario[3]
+    cc = vario[4]
+    a_max = vario[5]
+    a_min = vario[6]
+    
+    c = -nug # nugget effect is made negative because we're calculating covariance instead of variance
+    for i in range(nstruct):
+        rot_mat = Rot_Mat(Azimuth, a_max[i], a_min[i])
+
+        mat1 = cp.matmul(q1, rot_mat) 
+        mat2 = cp.matmul(q2.reshape(-1,2), rot_mat) 
+        # covariances between measurements and unknown
+        #d = pairwise_distances(mat1,mat2.T)
+        d = cp.sqrt(cp.square(mat1 - mat2).sum(axis=1)) # calculate distances
+        #print(d.shape)
+        #d = cp.sqrt(cp.square(mat1 - cp.tile(mat2,k)).sum(axis=1)) # calculate distances
+        #d = np.asarray(d).reshape(len(d))
+        c = c + covar(vtype[i], d, 1) * cc[i] # calculate covariances
+    return c
 
 # covariance model
 def cov(h1, h2, k, vario):
@@ -317,19 +374,19 @@ def sgsim(Pred_grid, df, xx, yy, data, k, vario, rad):
     :vario: variogram parameters describing the spatial statistics
     """
     
-    print('right version')
+    #print('right version')
     
     # generate random array for simulation order
     xyindex = np.arange(len(Pred_grid))
-    random.shuffle(xyindex)
+    random.Random(0).shuffle(xyindex) # random.shuffle(xyindex)
 
     Var_1 = cp.var(df[data].values); # variance of data 
     
     # preallocate space for simulation
     sgs = cp.zeros(shape=len(Pred_grid))
     
-    
     for i in tqdm(range(0, len(Pred_grid)), position=0, leave=True):
+    #for i in tqdm(range(0, 100), position=0, leave=True):
         z = xyindex[i]
 
         # convert data to numpy array for faster speeds/parsing
@@ -337,47 +394,43 @@ def sgsim(Pred_grid, df, xx, yy, data, k, vario, rad):
         # gather nearby points
         nearest = nearestNeighborSearch(rad, k, Pred_grid[z], df)
 
-        # store bed elevation values in new array
-        norm_bed_val = nearest[:,-1]
-        norm_bed_val = norm_bed_val.reshape(len(norm_bed_val), 1)
-        norm_bed_val = norm_bed_val.T
         # store X,Y pair values in new array
-        xy_val = nearest[:,:-1]
+        xy_val = nearest[["X","Y"]]
 
         # update K to reflect the amount of K values we got back from quadrant search
-        new_k = len(nearest)
+        new_k = nearest.shape[0]
 
 
         # left hand side (covariance between data)
-        Kriging_Matrix = np.zeros(shape=((new_k+1, new_k+1)))
-        Kriging_Matrix[0:new_k,0:new_k] = cov(xy_val, xy_val, 0, vario)
+        Kriging_Matrix = cp.zeros(shape=((new_k+1, new_k+1)))
+        Kriging_Matrix[0:new_k,0:new_k] = krig_cov(xy_val.values, vario)
         Kriging_Matrix[new_k,0:new_k] = 1
         Kriging_Matrix[0:new_k,new_k] = 1
 
         # Set up Right Hand Side (covariance between data and unknown)
-        r = np.zeros(shape=(new_k+1))
+        r = cp.zeros(shape=(new_k+1))
         k_weights = r
-        r[0:new_k] = cov(xy_val, np.tile(Pred_grid[z], (new_k, 1)), 1, vario)
+        r[0:new_k] = array_cov(xy_val.values, cp.tile(Pred_grid[z], new_k), vario) # covariance between simulation grid cell and conditioning data. cp.tile repeats simulation grid cell coordinate entries for covariance calculation
         r[new_k] = 1 # unbiasedness constraint
         Kriging_Matrix.reshape(((new_k+1)), ((new_k+1)))
-
+        
         # Calculate Kriging Weights
-        k_weights = np.dot(np.linalg.pinv(Kriging_Matrix), r)
+        k_weights = cp.dot(cp.linalg.pinv(Kriging_Matrix), r) # lambda = C^-1 * c
 
         # get estimates
-        est = np.sum(k_weights[0:new_k]*norm_bed_val[:]) # kriging mean
-        var = Var_1 - np.sum(k_weights[0:new_k]*r[0:new_k]) # kriging variance
-
+        est = cp.sum(k_weights[0:new_k]*nearest.Nbed.values) # kriging mean
+        var = Var_1 - cp.sum(k_weights[0:new_k]*r[0:new_k]) # kriging variance
+        
         if (var < 0): # make sure variances are non-negative
             var = 0 
+            
+        #print(est.shape,var.shape)
 
-        sgs[z] = np.random.normal(est,math.sqrt(var),1) # simulate by randomly sampling a value
+        sgs[z] = cp.random.normal(est,cp.sqrt(var),1) # simulate by randomly sampling a value
 
         # update the conditioning data
         coords = Pred_grid[z:z+1,:]
-        dnew = {xx: [coords[0,0]], yy: [coords[0,1]], data: [sgs[z]]} 
-        dfnew = pd.DataFrame(data = dnew)
-        df = pd.concat([df,dfnew], sort=False) # add new points by concatenating dataframes 
+        df = cudf.concat([df,cudf.DataFrame({"X": [coords[0,0]], "Y": [coords[0,1]], "Nbed": [sgs[z]], "Bed": cp.nan})], sort=False) # add new points by concatenating dataframes 
         
     return sgs
 
